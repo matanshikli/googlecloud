233305156dc43c56e27dc674117d96b4de358380
Matan Shikli <106749791+matanshikli@users.noreply.github.com>
2022-06-15 01:23:10 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,21 +8,6 @@ echo "Please select action to do "
 
 echo $space
 
-# select varName in List_instances  Delete_instances  Create_instance
-
-# do
-# case $varName in 
-#     List_instances)
-#         sudo gcloud compute instances list break;; 
-#     Delete_instances)
-#         sudo bash /Users/matanshikli/Desktop/gcloudui/list/.delete.sh break;;
-#     Create_instance)
-#         sudo bash /Users/matanshikli/Desktop/gcloudui/createvm/.createvm.sh break;;
-#     *)
-#         echo "Error select option 1..3";;
-    
-# esac            
-# done
 
 select tree in List_instances  Delete_instances  Create_instance 
 do
